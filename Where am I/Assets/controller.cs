e85da6d8ed589edfd16784ec4a8c9b57528dfa75
mfrancoisnantes <mfrancois.nantes@gmail.com>
2015-01-24 00:14:58 +0100

    controller v1

---
+++ Where am I/Assets/controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamepadInput;
+
+public class controller : MonoBehaviour {
+
+	public float speedMove = 10.0F;
+	public float speedRotate = 80.0F;
+
+	void Update(){
+		Vector2 leftStick = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any);
+		Vector2 rightStick = GamePad.GetAxis(GamePad.Axis.RightStick, GamePad.Index.Any);
+
+		transform.Translate(Vector3.forward * Time.deltaTime * leftStick.y * speedMove);
+		transform.Translate(Vector3.right * Time.deltaTime * leftStick.x * speedMove);
+		transform.Rotate(Vector3.up * Time.deltaTime * rightStick.x * speedRotate);
+
+		if (GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
+			Debug.Log ("A");
+	}
+}
