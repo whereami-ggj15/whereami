4bd744f9ba669a6473e7be26d2781b12610428c5
Geoffrey Hug <hug.geoffrey@gmail.com>
2015-01-23 23:28:08 +0100

    Mircophone

---
+++ Where am I/Assets/MicrophoneRecorder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneRecorder : MonoBehaviour {
+
+	AudioSource aud;
+	bool isRecording = false;
+
+	int minFreq = 0;
+	int maxFreq = 0;
+
+	void Start(){
+		aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+		aud.Play();
+		Microphone.GetDeviceCaps(string.Empty, minFreq, maxFreq);
+		Debug.Log(minFreq + " " + maxFreq);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1") && !isRecording){
+			isRecording = true;
+		}
+	}
+}
