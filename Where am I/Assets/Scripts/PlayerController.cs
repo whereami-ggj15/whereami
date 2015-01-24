611e176bc9a6e866d294bc093e9537ab78cda100
mfrancoisnantes <mfrancois.nantes@gmail.com>
2015-01-24 13:00:38 +0100

    jet de pierre

--- Where am I/Assets/Scripts/PlayerController.cs
+++ Where am I/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,24 @@ public class PlayerController : MonoBehaviour {
 	public KeyCode turnLeft = KeyCode.Q;
 	public KeyCode strafeRight = KeyCode.E;
 	public KeyCode strafeLeft = KeyCode.A;
-
-	public float speedMove = 5000.0f;
-	public float speedRotate = 80.0f;
-	public float joystickTolerance = 0.2F;
+	public KeyCode walkToggle = KeyCode.LeftShift;
+	public KeyCode throwRock = KeyCode.Space;
+	
+	private float speedMove = 10.0f;
+	private float speedRotate = 80.0f;
+	private float joystickTolerance = 0.2F;
+	private float detectionTrigger = 1.0F;
+	private float throwRockForce = 1000.0F;
 
 	void FixedUpdate(){
-		JoysticksManager();
+		GamePadManager();
 		KeyboardManager();
 
 		if (GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
 			Debug.Log ("A");
 	}
 
-	private void JoysticksManager(){
+	private void GamePadManager(){
 		float tolerence = joystickTolerance;
 		Vector2 leftStick = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any);
 		Vector2 rightStick = GamePad.GetAxis(GamePad.Axis.RightStick, GamePad.Index.Any);
@@ -43,20 +47,30 @@ public class PlayerController : MonoBehaviour {
 				tolerence = -tolerence;
 			transform.Rotate(Vector3.up * Time.deltaTime * (rightStick.x - tolerence) * speedRotate);
 		}
+		if (GamePad.GetTrigger(GamePad.Trigger.RightTrigger, GamePad.Index.Any) >= detectionTrigger)
+			rock.instanciateAndThrowRock(throwRockForce);
 	}
 
 	private void KeyboardManager(){
+		float currSpeedMove;
+
+		if (Input.GetKey (walkToggle))
+			currSpeedMove = speedMove / 2;
+		else
+			currSpeedMove = speedMove;
 		if (Input.GetKey (forward))
-			transform.Translate(Vector3.forward * Time.deltaTime * speedMove);
+			transform.Translate(Vector3.forward * Time.deltaTime * currSpeedMove);
 		if (Input.GetKey (back))
-			transform.Translate(Vector3.back * Time.deltaTime * speedMove);
+			transform.Translate(Vector3.back * Time.deltaTime * currSpeedMove);
 		if (Input.GetKey (strafeLeft))
-			transform.Translate(Vector3.left * Time.deltaTime * speedMove);
+			transform.Translate(Vector3.left * Time.deltaTime * currSpeedMove);
 		if (Input.GetKey (strafeRight))
-			transform.Translate(Vector3.right * Time.deltaTime * speedMove);
+			transform.Translate(Vector3.right * Time.deltaTime * currSpeedMove);
 		if (Input.GetKey (turnLeft))
 			transform.Rotate(Vector3.up * Time.deltaTime * -speedRotate);
 		if (Input.GetKey (turnRight))
 			transform.Rotate(Vector3.up * Time.deltaTime * speedRotate);
+		if (Input.GetKey (throwRock))
+			rock.instanciateAndThrowRock(throwRockForce);
 	}
 }
---
+++ Where am I/Assets/Scripts/rock.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rock : MonoBehaviour {
+
+	private static bool isThrowing = false;
+	private static GameObject rockGameObject;
+	private static float highSpawnRock = 2.5F;
+	private static string objectRockName = "Rock";
+	private static string obejctPlayerName = "Player";
+
+	public static void instanciateAndThrowRock(float force){
+		if (!isThrowing){
+			Debug.Log("Player is throwing a rock !");
+			rock rockScript;
+			Transform playerTransform = GameObject.Find(obejctPlayerName).transform;
+			
+			isThrowing = true;
+			rockGameObject = (GameObject)Instantiate((GameObject)Resources.Load(objectRockName));
+			rockScript = rockGameObject.GetComponent<rock>();
+			Vector3 tmpPos = playerTransform.localPosition;
+			tmpPos.y = highSpawnRock;
+			rockGameObject.transform.position = tmpPos;
+			rockGameObject.transform.rotation = playerTransform.localRotation;
+			rockScript.rigidbody.AddForce(playerTransform.TransformDirection(Vector3.forward) * force);
+		}
+	}
+
+	private void OnCollisionEnter(Collision ground){
+		Debug.Log("Rock collision !");
+	}
+
+	private void Update(){
+		if (rockGameObject.rigidbody.velocity.magnitude <= 0.0F){
+			Debug.Log ("rock depop from the world");
+			isThrowing = false;
+			Destroy(rockGameObject);
+			rockGameObject = null;
+		}
+	}
+}
