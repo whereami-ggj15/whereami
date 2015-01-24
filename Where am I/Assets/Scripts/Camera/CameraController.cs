dbdf528af4d0c6428a207c067f66030f90da157f
tspoke <tspoke@hotmail.fr>
2015-01-24 23:40:57 +0100

    Clean merge

--- Where am I/Assets/Scripts/Camera/CameraController.cs
+++ Where am I/Assets/Scripts/Camera/CameraController.cs
@@ -2,8 +2,19 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-
-	void Update(){
-
+	
+	public Transform target;
+	public float smoothing = 5f;
+	
+	Vector3 offset; //distance between player/camera
+	
+	void Start(){
+		transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+		offset = transform.position - target.position;
+	}
+	
+	void FixedUpdate(){
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 	}
 }
