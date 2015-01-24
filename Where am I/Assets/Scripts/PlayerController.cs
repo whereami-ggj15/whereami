520e49f9df8ec16ccb9441b740b52b80d135aa54
tspoke <tspoke@hotmail.fr>
2015-01-24 02:15:49 +0100

    editing movements

--- Where am I/Assets/Scripts/PlayerController.cs
+++ Where am I/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,28 @@ using GamepadInput;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speedMove = 10.0F;
-	public float speedRotate = 80.0F;
+	public float speedMove = 1000.0f;
+	public float speedRotate = 80.0f;
 
-	void Update(){
+	private Rigidbody rigidbody;
+
+	void Awake(){
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
 		Vector2 leftStick = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any);
 		Vector2 rightStick = GamePad.GetAxis(GamePad.Axis.RightStick, GamePad.Index.Any);
 
+		/*
 		transform.Translate(Vector3.forward * Time.deltaTime * leftStick.y * speedMove);
 		transform.Translate(Vector3.right * Time.deltaTime * leftStick.x * speedMove);
 		transform.Rotate(Vector3.up * Time.deltaTime * rightStick.x * speedRotate);
+		*/
+
+		Vector3 force = new Vector3(leftStick.x, 0.0f, leftStick.y);
+		rigidbody.AddForce(force * speedMove * Time.deltaTime);
+		//rigidbody.rotation (new Quaternion());
 
 		if (GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
 			Debug.Log ("A");
