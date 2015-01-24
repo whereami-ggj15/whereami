f5d9840a7c1390fcbc6347e6b1f6dee5254789b4
tspoke <tspoke@hotmail.fr>
2015-01-24 09:23:13 +0100

    first day two

--- Where am I/Assets/Scripts/PlayerController.cs
+++ Where am I/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using GamepadInput;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speedMove = 1000.0f;
+	public float speedMove = 5000.0f;
 	public float speedRotate = 80.0f;
 
 	private Rigidbody rigidbody;
@@ -17,15 +17,25 @@ public class PlayerController : MonoBehaviour {
 		Vector2 leftStick = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.Any);
 		Vector2 rightStick = GamePad.GetAxis(GamePad.Axis.RightStick, GamePad.Index.Any);
 
+		Quaternion rotation = transform.localRotation;
+		rotation.x += rightStick.x;
+		rotation.y = rightStick.y;
+
+
 		/*
 		transform.Translate(Vector3.forward * Time.deltaTime * leftStick.y * speedMove);
 		transform.Translate(Vector3.right * Time.deltaTime * leftStick.x * speedMove);
-		transform.Rotate(Vector3.up * Time.deltaTime * rightStick.x * speedRotate);
-		*/
 
-		Vector3 force = new Vector3(leftStick.x, 0.0f, leftStick.y);
-		rigidbody.AddForce(force * speedMove * Time.deltaTime);
-		//rigidbody.rotation (new Quaternion());
+		*/
+		transform.Rotate(Vector3.up * Time.deltaTime * rightStick.x * speedRotate);
+		Vector3 direction = transform.worldToLocalMatrix.MultiplyVector(transform.forward);
+		direction.x += leftStick.x;
+		direction.z += leftStick.y;
+
+		//Vector3 force = new Vector3(leftStick.x, 0.0f, leftStick.y);
+		rigidbody.AddForce(direction * speedMove * Time.deltaTime);
+
+		Quaternion.Lerp(transform.rotation, rotation, 10 * Time.deltaTime);
 
 		if (GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
 			Debug.Log ("A");
