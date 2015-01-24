4cd840dd093efda5fc1c6d35bfc442d4f145aa64
tspoke <tspoke@hotmail.fr>
2015-01-24 16:54:45 +0100

    Cleaning workspace

---
+++ Where am I/Assets/Scripts/Player/PlaySoundOnEnter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnEnter : MonoBehaviour {
+
+	AudioSource source;
+
+	void Start(){
+		source = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player"){
+			if(!source.isPlaying){
+				source.Play();
+			}
+		}
+	}
+}
---
+++ Where am I/Assets/Scripts/Player/PlayerKeyboardController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKeyboardController : MonoBehaviour {
+
+	public float speedMove = 5000.0f;
+	public float speedRotate = 80.0f;
+	
+	void FixedUpdate(){
+		
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		//Debug.Log(new Vector2(horizontal, vertical));
+		
+		Quaternion rotation = transform.localRotation;
+		rotation.y += horizontal;
+		
+		
+		/*
+		transform.Translate(Vector3.forward * Time.deltaTime * leftStick.y * speedMove);
+		transform.Translate(Vector3.right * Time.deltaTime * leftStick.x * speedMove);
+
+		*/
+		transform.Rotate(Vector3.up * Time.deltaTime * horizontal * speedRotate);
+		Quaternion.Lerp(transform.rotation, rotation, 10 * Time.deltaTime);
+
+		Vector3 direction = transform.forward * vertical;
+
+		//Debug.Log(rigidbody.velocity);
+		//Vector3 force = new Vector3(leftStick.x, 0.0f, leftStick.y);
+		rigidbody.AddForce(direction * speedMove * Time.deltaTime);
+		
+
+
+	}
+}
