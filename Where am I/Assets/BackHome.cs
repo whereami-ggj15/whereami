408dbf0ddb8ffa673eaae3dd10f6727543e4b680
tspoke <tspoke@hotmail.fr>
2015-01-25 13:58:14 +0100

    Before merging

---
+++ Where am I/Assets/BackHome.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackHome : MonoBehaviour {
+
+	void Update () {
+		if (Input.anyKeyDown){
+			Application.LoadLevel("Home");
+		}
+	}
+
+}
